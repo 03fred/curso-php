92322f7cb2d8476d470af8bf8793f9f220fa0152
Fred <frederico.d.silva@outlook.com>
2019-10-09 20:51:57 -0300

    implementacao do form

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+        <meta charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+<div class="form-group">
+        <label>Digite o número do Produto </label>
+        <input type="number" class="form-control" name="productNumber" placeholder="Número do Produto"/>
+</div>
+
+<div class="form-group">
+        <label>Digite o nome do Produto </label>
+        <input type="text" class="form-control" name="productName" placeholder="Nome do Produto"/>
+</div>
+
+<div class="form-group">
+        <label>Digite o nome do Produto </label>
+        <input type="number" class="form-control" name="productNumber" placeholder="Nome do Produto"/>
+</div>
+
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
